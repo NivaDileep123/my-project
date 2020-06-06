f16a9fc90495c4e2379a0c4d722cc683eba1107c
Niva Dileep <niva.cs17@jecc.ac.in>
2020-06-06 08:40:05 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+function displayfd(){
+  document.getElementById('frame').style.backgroundImage="url('indian.jpg')";
+  document.getElementById('frame').style.innerHTML="Indian food";
+}
+function nodisplayfd(){
+  document.getElementById('frame').style.backgroundImage="url('')";
+  document.getElementById('frame').style.innerHTML='Hover over an button to see sample image';
+}
